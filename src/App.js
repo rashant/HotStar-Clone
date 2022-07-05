fb4e42092d6473a097dfd7e7745e77daec95d41d
Prashant <invokeai.labs@gmail.com>
2022-07-05 10:39:10 +0530

    Initialize project using Create React App

--- src/App.js
+++ src/App.js
@@ -55,6 +55,7 @@ function App() {
     <div className="watch">
     <span>Memes</span>
     </div>
+
     <div className="media-row">
     <Media img="https://1.bp.blogspot.com/-CvQm8Vpw89M/XpPCiqTHxII/AAAAAAAAHXE/Ki6YIEUv9QouiEc9RcNBrDO210BiG5ftgCLcBGAsYHQ/s1600/Pokemon%2BSeason%2B17%2BThe%2BSeries%2BXY%2BAll%2BEpisodes%2Bin%2BHD.jpeg"/>
     <Media img="https://1.bp.blogspot.com/-CvQm8Vpw89M/XpPCiqTHxII/AAAAAAAAHXE/Ki6YIEUv9QouiEc9RcNBrDO210BiG5ftgCLcBGAsYHQ/s1600/Pokemon%2BSeason%2B17%2BThe%2BSeries%2BXY%2BAll%2BEpisodes%2Bin%2BHD.jpeg"/>
